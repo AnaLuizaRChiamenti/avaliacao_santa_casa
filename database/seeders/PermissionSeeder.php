d9ae308cb17576b00330c43ae3334305dfb1053d
Ana Chiamenti <analuiza@justnew.com.br>
2026-07-04 22:46:26 -0300

    feat: configura permissões iniciais dos módulos

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -10,10 +10,10 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         $permissions = [
-            ['name' => 'Módulo Financeiro', 'slug' => 'financeiro'],
-            ['name' => 'Módulo Estoque', 'slug' => 'estoque'],
-            ['name' => 'Módulo Relatórios', 'slug' => 'relatorios'],
-            ['name' => 'Módulo Configurações', 'slug' => 'configuracoes'],
+            ['name' => 'Setores Hospitalares', 'slug' => 'setores-hospitalares'],
+            ['name' => 'Especialidades Médicas', 'slug' => 'especialidades-medicas'],
+            ['name' => 'Equipamentos', 'slug' => 'equipamentos'],
+            ['name' => 'Unidades Assistenciais', 'slug' => 'unidades-assistenciais'],
         ];
 
         foreach ($permissions as $permission) {
